2283e65e5832328471cb0b1723e89d9043451fe0
Joseph K <joemaren@users.noreply.github.com>
2020-11-06 07:53:17 +0100

    created conversion logic from base 2 to 10

--- app/src/main/java/com/bignerdranch/android/bindec/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/bindec/MainActivity.kt
@@ -2,10 +2,30 @@ package com.bignerdranch.android.bindec
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bin_input: EditText = findViewById(R.id.binary_input)
+        val decimal_textview: TextView = findViewById(R.id.decimal_textview)
+        val convert_btn: Button = findViewById(R.id.convert_btn)
+
+
+        bin_input.transformationMethod = null
+
+        convert_btn.setOnClickListener {_: View ->
+            decimal_textview.text = (Integer.parseInt(bin_input.text.toString(),2)).toString()
+        }
+        decimal_textview.text=bin_input.text
+
+
     }
 }
